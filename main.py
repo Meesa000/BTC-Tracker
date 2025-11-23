8daa0b4a7d9debf84d40aa08fae41d9ba339d765
Meesa000 <104768879+Meesa000@users.noreply.github.com>
2025-11-23 18:04:09 +0000

    format last updated

--- main.py
+++ main.py
@@ -19,9 +19,11 @@ def display_data():
     pct_change_30d = f"{pct_change_30d:.2f}"
     pct_change_60d = f"{pct_change_60d:.2f}"
     pct_change_90d = f"{pct_change_90d:.2f}"
+    last_updated = last_updated[:-5]
+    last_updated_formatted = last_updated.replace("T", " ")
     
     
-    return render_template('index.html', name=name, max_supply=max_supply, total_supply=total_supply, gbp_price=gbp_price, last_updated=last_updated,
+    return render_template('index.html', name=name, max_supply=max_supply, total_supply=total_supply, gbp_price=gbp_price, last_updated=last_updated_formatted,
                            supply_left=supply_left, pct_change_1h=pct_change_1h, pct_change_24h=pct_change_24h, pct_change_7d=pct_change_7d, pct_change_30d=pct_change_30d,
                            pct_change_60d=pct_change_60d, pct_change_90d= pct_change_90d)
 
